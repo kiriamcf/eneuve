9bc591e04c3915924ca459cad86d12806d82357c
kiriamcf <kiriamcf@gmail.com>
2025-03-27 22:20:38 +0100

    added astro base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kiriamcf.github.io',
+  base: '/eneuve/',
   vite: {
     plugins: [tailwindcss()]
   }
